51805334c35720e046df898bb058184050c8f6d4
Stefani <stefani.barros28@gmail.com>
2021-09-30 22:54:29 -0300

    adicionando arquivos Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MiddlePageComponent } from './components/middle-page/middle-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiddlePageComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/middle-page/middle-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-middle-page',
+  templateUrl: './middle-page.component.html',
+  styleUrls: ['./middle-page.component.scss']
+})
+export class MiddlePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
